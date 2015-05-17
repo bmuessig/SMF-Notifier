cbc639605c1eecd14b2d89524e09bdf2ecf63755
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-05-17 17:01:02 +0200

    Changed folder layout again and added server code

---
+++ src-server/walrus_notify.php
@@ -0,0 +1,211 @@
+<?php
+ob_start();
+
+// Constants
+define("BASE_URL_CW_FEED", "http://codewalr.us/index.php?action=.xml");
+define("TMP_DB_FILE", "muessigbnet_api-walrus.notify");
+define("CACHE_TIME", 40);
+define("CACHE_POSTS", 200);
+
+$cached = false;
+if(file_exists(TMP_DB_FILE)) {
+	try {
+		$cacheRawObj = file_get_contents(TMP_DB_FILE);
+		if(isJson($cacheRawObj)) {
+			$cacheObj = json_decode($cacheRawObj);
+			if( isset($cacheObj->timestamp) && isset($cacheObj->data) ) {
+				if( time() < (intval($cacheObj->timestamp) + CACHE_TIME) ) {
+					$cache = strval($cacheObj->data);
+					$cached = true;
+				}
+			}
+		}
+	} catch (Exception $ex) {
+		$cacheJson = false;
+	}
+}
+
+if(! $cached) {
+	$queryOpts = array("sa=recent", "limit" . CACHE_POSTS);
+	$rawInput = QueryFeed(BASE_URL_CW_FEED, $queryOpts);
+	file_put_contents(TMP_DB_FILE, json_encode(array("timestamp" => time(), "data" => $rawInput)));
+} else {
+	$rawInput = $cache;
+}
+
+$posts = UltraWalriiParser($rawInput);
+$json = json_encode(array(	"success"	=> true,
+							"cached"	=> $cached,
+							"data"		=> $posts,
+					));
+
+header('Content-Type: application/json');
+print(json_minify($json));
+
+print_gzipped_output();
+
+function GetFeed() {
+
+}
+
+function UltraWalriiParser($rawXml)
+{
+	try {
+		$recentPosts = (new SimpleXMLElement($rawXml));
+		$outputData = array();
+		
+		foreach ($recentPosts->{'recent-post'} as $post) {
+			array_push($outputData,
+						array(	"post"		=> array(	"subject"	=> StripHtml(strval($post->subject)),
+														"body"		=> StripHtml(strval($post->body)),
+														"id"		=> intval($post->id),
+														"time"		=> ToUnixTime(strval($post->time)),
+														"link"		=> strval($post->link)
+												),
+								"poster"	=> array(	"name"	=> strval($post->poster->name),
+														"id"	=> intval($post->poster->id),
+														"link"	=> strval($post->poster->link)
+													),
+								"topic"		=> array(	"subject"	=> strval($post->topic->subject),
+														"id"		=> intval($post->topic->id),
+														"link"		=> strval($post->topic->link)
+													),
+								"starter"	=> array(	"name"		=> strval($post->starter->name),
+														"id"		=> intval($post->starter->id),
+														"link"		=> strval($post->starter->link)
+													),
+								"board"		=> array(	"name"		=> strval($post->board->name),
+														"id"		=> intval($post->board->id),
+														"link"		=> strval($post->board->link)
+													)
+							));
+		}
+		
+		return $outputData;
+	} catch (Exception $ex) {
+		echo $ex;
+	}
+}
+
+function ToUnixTime($smfStr)
+{
+	// Convert to unix time
+	return strtotime(str_replace(" at ", ", ", $smfStr));
+}
+
+function StripHtml($str)
+{
+	// Handle line breaks
+	$outStr = preg_replace("/<br \/>/", "\n", $str);
+	// Handle smileys
+	$outStr = preg_replace("/<img src=\"Smileys\/[^\n\"\=]*\" alt=\"([^\n\"\=]*)\" title=\"[^\n\"\=]*\" class=\"smiley\" \/>/", "$1", $outStr);
+	// Strip all remaining Html tags
+	$outStr = preg_replace("/<(\/)?[^\n<>]*( \/)?>/", "", $outStr);
+	// Replace html entitys
+	$outStr = html_entity_decode($outStr, (ENT_QUOTES | ENT_HTML401));
+	
+	return $outStr;
+}
+
+function QueryFeed($baseUrl, $opts)
+{
+	// Concernate the different pieces together, glue them with ;
+	$query = implode(";", $opts);
+	// Send request to the server and retrieve answer
+	return file_get_contents($baseUrl . (strlen($query)>0 ? (";" . $query) : ""));
+}
+
+function print_gzipped_output() 
+{ 
+	if(! isset($_SERVER["HTTP_ACCEPT_ENCODING"]) )
+		$encoding = false;
+	else {
+		$HTTP_ACCEPT_ENCODING = $_SERVER["HTTP_ACCEPT_ENCODING"]; 
+		if( headers_sent() ) 
+			$encoding = false; 
+		else if( strpos($HTTP_ACCEPT_ENCODING, 'x-gzip') !== false ) 
+			$encoding = 'x-gzip'; 
+		else if( strpos($HTTP_ACCEPT_ENCODING,'gzip') !== false ) 
+			$encoding = 'gzip'; 
+		else 
+			$encoding = false;
+	}
+    
+    if( $encoding ) 
+    { 
+        $contents = ob_get_clean(); 
+        $_temp1 = strlen($contents); 
+        if ($_temp1 < 2048)    // no need to waste resources in compressing very little data 
+            print($contents); 
+        else 
+        { 
+            header('Content-Encoding: '.$encoding); 
+            print("\x1f\x8b\x08\x00\x00\x00\x00\x00"); 
+            $contents = gzcompress($contents, 9); 
+            $contents = substr($contents, 0, $_temp1); 
+            print($contents); 
+        } 
+    } 
+    else 
+        ob_end_flush(); 
+}
+
+/*! JSON.minify()
+	v0.1 (c) Kyle Simpson
+	MIT License
+*/
+function json_minify($json) {
+	$tokenizer = "/\"|(\/\*)|(\*\/)|(\/\/)|\n|\r/";
+	$in_string = false;
+	$in_multiline_comment = false;
+	$in_singleline_comment = false;
+	$tmp; $tmp2; $new_str = array(); $ns = 0; $from = 0; $lc; $rc; $lastIndex = 0;
+		
+	while (preg_match($tokenizer,$json,$tmp,PREG_OFFSET_CAPTURE,$lastIndex)) {
+		$tmp = $tmp[0];
+		$lastIndex = $tmp[1] + strlen($tmp[0]);
+		$lc = substr($json,0,$lastIndex - strlen($tmp[0]));
+		$rc = substr($json,$lastIndex);
+		if (!$in_multiline_comment && !$in_singleline_comment) {
+			$tmp2 = substr($lc,$from);
+			if (!$in_string) {
+				$tmp2 = preg_replace("/(\n|\r|\s)*/","",$tmp2);
+			}
+			$new_str[] = $tmp2;
+		}
+		$from = $lastIndex;
+			
+		if ($tmp[0] == "\"" && !$in_multiline_comment && !$in_singleline_comment) {
+			preg_match("/(\\\\)*$/",$lc,$tmp2);
+			if (!$in_string || !$tmp2 || (strlen($tmp2[0]) % 2) == 0) {	// start of string with ", or unescaped " character found to end string
+				$in_string = !$in_string;
+			}
+			$from--; // include " character in next catch
+			$rc = substr($json,$from);
+		}
+		else if ($tmp[0] == "/*" && !$in_string && !$in_multiline_comment && !$in_singleline_comment) {
+			$in_multiline_comment = true;
+		}
+		else if ($tmp[0] == "*/" && !$in_string && $in_multiline_comment && !$in_singleline_comment) {
+			$in_multiline_comment = false;
+		}
+		else if ($tmp[0] == "//" && !$in_string && !$in_multiline_comment && !$in_singleline_comment) {
+			$in_singleline_comment = true;
+		}
+		else if (($tmp[0] == "\n" || $tmp[0] == "\r") && !$in_string && !$in_multiline_comment && $in_singleline_comment) {
+			$in_singleline_comment = false;
+		}
+		else if (!$in_multiline_comment && !$in_singleline_comment && !(preg_match("/\n|\r|\s/",$tmp[0]))) {
+			$new_str[] = $tmp[0];
+		}
+	}
+	$new_str[] = $rc;
+	return implode("",$new_str);
+}
+
+function isJson($string) {
+	json_decode($string);
+	return (json_last_error() == JSON_ERROR_NONE);
+}
+
+?>

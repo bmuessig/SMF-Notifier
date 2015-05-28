92361e85b9b8110b6e9feab52a4b74dc082e9bc9
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-05-29 00:45:02 +0200

    More work on settings

--- src-client/CodeWalriiNotify/SettingsProvider.cs
+++ src-client/CodeWalriiNotify/SettingsProvider.cs
@@ -32,7 +32,94 @@ namespace CodeWalriiNotify
 
 
 		}
-		
+
+		public static string FeedURL {
+			get;
+			set;
+		}
+
+		public static string ApplicationTitle {
+			get;
+			set;
+		}
+
+		public static uint QueryInterval {
+			get;
+			set;
+		}
+
+		public static byte MaximumPosts {
+			get;
+			set;
+		}
+
+		public static bool AudioNotify {
+			get;
+			set;
+		}
+
+		public static bool VisualNotify {
+			get;
+			set;
+		}
+
+		public static Gdk.Color HeaderBackcolor {
+			get;
+			set;
+		}
+
+		public static Gdk.Color TimestampForecolor {
+			get;
+			set;
+		}
+
+		public static Gdk.Color AuthorForecolor {
+			get;
+			set;
+		}
+
+		public static Gdk.Color TitleForecolor {
+			get;
+			set;
+		}
+
+		public static Gdk.Color BodyForecolor {
+			get;
+			set;
+		}
+
+		public static Gdk.Color BodyBackcolor {
+			get;
+			set;
+		}
+
+		public static string TitleFont {
+			get;
+			set;
+		}
+
+		public static string DetailFont {
+			get;
+			set;
+		}
+
+		public static string BodyFont {
+			get;
+			set;
+		}
+			
+		/*
+		var headerBackcolor = new Color(110, 180, 137);
+		var timeForecolor = new Color(216, 216, 216);
+		var authorForecolor = new Color(198, 198, 198);
+		var titleForecolor = new Color(255, 255, 255);
+		var bodyBackcolor = new Color(250, 250, 250);
+		var bodyForecolor = new Color(0, 0, 0);
+
+		var titleFont = Pango.FontDescription.FromString("Tahoma 15.6");
+		var detailFont = Pango.FontDescription.FromString("Tahoma 10.5");
+		var bodyFont = Pango.FontDescription.FromString("Tahoma 13.6");
+		*/
 	}
 }
 

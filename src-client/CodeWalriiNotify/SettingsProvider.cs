97db72cb3d139a193d543a7b6fe9101b5c72965a
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-05-29 00:28:37 +0200

    Work on settings

--- src-client/CodeWalriiNotify/SettingsProvider.cs
+++ src-client/CodeWalriiNotify/SettingsProvider.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.IO;
+using System.Web.Script.Serialization;
 
 namespace CodeWalriiNotify
 {
-	public class SettingsProvider
+	public static class SettingsProvider
 	{
-		public SettingsProvider()
+		static JavaScriptSerializer jsonDecoder;
+
+		static SettingsProvider()
 		{
+			FromFile("config.json");
 		}
+
+
+		public static void FromFile(string Path, bool DefaultOnError = true)
+		{
+			string rawSettingsJson;
+
+			if (!File.Exists(Path)) {
+				if (!DefaultOnError)
+					throw new FileNotFoundException("The JSON file couldn't be found and defaulting is disabled!", Path);
+				else
+					rawSettingsJson = ""; // default json settings
+			} else
+				rawSettingsJson = File.ReadAllText(Path);
+			
+			jsonDecoder = new JavaScriptSerializer();
+			jsonDecoder.RegisterConverters(new[] { new DynamicJsonConverter() });
+			dynamic jsonObj = jsonDecoder.Deserialize(rawSettingsJson, typeof(object));
+
+
+		}
+		
 	}
 }
 

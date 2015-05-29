1e4e925e8eb4bab57740a29e361795f3920e379f
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-05-30 00:34:27 +0200

    API query class created

---
+++ src-client/CodeWalriiNotify/APIQueryMeta.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CodeWalriiNotify
+{
+	public class APIQueryMeta
+	{
+		public APIQueryMeta(string JSON)
+		{
+			
+		}
+
+		public APIQueryMeta(bool Success, bool Cached, uint Timestamp, uint Changed, string[] Exceptions, PostMeta[] Posts)
+		{
+			this.Success = Success;
+			this.Cached = Cached;
+			this.Timestamp = Timestamp;
+			this.Changed = Changed;
+			this.Exceptions = Exceptions;
+			this.Posts = Posts;
+		}
+
+		public uint Timestamp {
+			get;
+			private set;
+		}
+
+		public uint Changed {
+			get;
+			private set;
+		}
+
+		public bool Success {
+			get;
+			private set;
+		}
+
+		public bool Cached {
+			get;
+			private set;
+		}
+
+		public string[] Exceptions {
+			get;
+			private set;
+		}
+
+		public PostMeta[] Posts {
+			get;
+			private set;
+		}
+	}
+}
+

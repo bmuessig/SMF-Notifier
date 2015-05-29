a46b2ec94b68ce3a3929e61fa57e46e317be3e9a
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-05-29 19:46:52 +0200

    Say hello to our new HTML Renderer

---
+++ src-client/CodeWalriiNotify/HTMLRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using TheArtOfDev.HtmlRenderer.WinForms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace CodeWalriiNotify
+{
+	public static class HTMLRenderer
+	{
+		public static Gdk.Pixbuf RenderHTML(string HTML, uint Width, uint Height, bool UseAntiAlias = true)
+		{
+			Image img = HtmlRender.RenderToImageGdiPlus(HTML, (int)Width, (int)Height, UseAntiAlias ? System.Drawing.Text.TextRenderingHint.AntiAlias : System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit);
+			return ImageToPixbuf(img);
+		}
+
+		static Gdk.Pixbuf ImageToPixbuf(Image image)
+		{ 
+			using (var stream = new MemoryStream()) { 
+				image.Save(stream, ImageFormat.Png); 
+				stream.Position = 0; 
+				return new Gdk.Pixbuf(stream); 
+			} 
+		}
+	}
+}
+

5e22bdb32e48e5122bd9572d14c2f22a24aaabcf
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-05-30 16:43:24 +0200

    Created settings dialog window

---
+++ src-client/CodeWalriiNotify/SettingsDialog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CodeWalriiNotify
+{
+	public partial class SettingsDialog : Gtk.Dialog
+	{
+		public SettingsDialog()
+		{
+			this.Build();
+		}
+	}
+}
+
---
+++ src-client/CodeWalriiNotify/gtk-gui/CodeWalriiNotify.SettingsDialog.cs
@@ -0,0 +1,10 @@
+
+namespace CodeWalriiNotify
+{
+	public partial class SettingsDialog
+	{
+		private Gtk.Button buttonCancel;
+		
+		private Gtk.Button buttonOk;
+	}
+}
